79f80e7020ca119168059619358a5d8ef2017899
ahmed-naway <75022119+ahmed-naway@users.noreply.github.com>
2024-11-18 21:19:52 +0400

    responsive apdate

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+
+
+const addRemoveClass = (className) => {
+  if(menuBtn.classList.contains(className)){
+    menuBtn.classList.remove(className);
+  } else {
+    menuBtn.classList.add(className);
+  }
+};
+
+const addRemoveActive = (className) => {
+    if(menu.classList.contains(className)){
+      menu.classList.remove(className);
+    } else {
+      menu.classList.add(className);
+    }
+  };
+
+menuBtn.addEventListener("click", () => addRemoveClass('close'));
+menuBtn.addEventListener("click", () => addRemoveActive('active'));
