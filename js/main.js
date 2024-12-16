f28a06f38ebba5d0bf83ce4bd127402cce0b5ebb
ahmed-naway <75022119+ahmed-naway@users.noreply.github.com>
2024-12-16 15:38:28 +0400

    buttons function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
+const filter = document.querySelectorAll("#filter li");
 
 const addRemoveClass = (className) => {
   if (menuBtn.classList.contains(className)) {
@@ -9,13 +10,23 @@ const addRemoveClass = (className) => {
   }
 };
 
-const addRemoveActive = (className) => {
-  if (menu.classList.contains(className)) {
-    menu.classList.remove(className);
-  } else {
-    menu.classList.add(className);
-  }
-};
+filter.forEach((gif) => {
+  gif.addEventListener("click", function (e) {
+    const sipling = gif.parentElement.children;
+
+    for (let i = 0; i < sipling.length; i++) {
+      if (sipling[i].classList.contains("active")) {
+        sipling[i].classList.remove("active");
+      } else {
+        continue;
+      }
+    }
+
+    if (gif.classList != "active") {
+      gif.classList.add("active");
+    }
+  });
+});
 
 menuBtn.addEventListener("click", () => addRemoveClass("close"));
-menuBtn.addEventListener("click", () => addRemoveActive("active"));
+menuBtn.addEventListener("click", () => addRemoveClass("active"));
